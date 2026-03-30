66381ccdb05dfc795698a930ba7d67aae30658df
Lynxify-Agency <321296056+Lynxify-Agency@users.noreply.github.com>
2026-03-30 16:08:49 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import ShopCategory from './Pages/ShopCategory';
+import Product from './Pages/Product';
+import LoginSignup from './Pages/LoginSignup';
+import Shop from './Pages/Shop';
+import Footer from './Components/Footer/Footer';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+     <Navbar></Navbar>
+     <Routes>
+      <Route path='/' element={<Shop/>}/>
+      <Route path='/mens' element={<ShopCategory category="men"/>}/>
+      <Route path='/womens' element={<ShopCategory category="women"/>}/>
+      <Route path='/kids' element={<ShopCategory category="kid"/>} />
+      <Route path='/product' element={<Product/>}></Route>
+        <Route path=':productId' element={<Product/>}></Route>  
+        <Route path='/cart' element={<cart/>}></Route>
+        <Route path='/login' element={<LoginSignup/>}></Route>
+     </Routes>
+     <Footer></Footer>
+      </BrowserRouter>
     </div>
   );
 }
