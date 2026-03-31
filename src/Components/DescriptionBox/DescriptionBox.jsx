91c8788016a110cad5c8a53d265a25e12a26c788
Lynxify-Agency <321296056+Lynxify-Agency@users.noreply.github.com>
2026-03-31 16:39:32 +0500

    fresh site

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto perferendis harum sapiente dolorum saepe illum quisquam alias ab aspernatur eligendi consequatur quo sit dolores quod iste ut, accusamus nam omnis? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab dignissimos, ducimus temporibus architecto harum expedita quisquam nemo ipsam tempore, sunt officiis mollitia quidem natus numquam excepturi ipsum velit omnis aliquam?
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut harum, quo assumenda in ipsam quis enim ad fugiat cum eius reiciendis sint, magnam ab, illo deserunt earum amet veniam. Excepturi.
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -3,6 +3,7 @@ import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom'
 import Breadcrum from '../Components/Breadcrums/Breadcrum'
 import ProductDisplay from '../Components/ProductDisplay/ProductDisplay'
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox'
 
 const Product = () => {
   const { all_product } = useContext(ShopContext)
@@ -20,6 +21,7 @@ const Product = () => {
     <div>
       <Breadcrum product={product} />
       <ProductDisplay product={product} />
+      <DescriptionBox></DescriptionBox>
     </div>
   )
 }
